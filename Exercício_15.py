b68594c952099d50221d3fa5869bfb08d2c21c25
Igor Bragança <igor.braganca2003@gmail.com>
2021-09-16 23:18:35 -0300

    Add files via upload

---
+++ Exercício_15.py
@@ -0,0 +1,62 @@
+'''Programa que pega os dados de entrada como código
+da região do cliente, seu nome, o nome de seu vendedor,
+o número de peças compradas, calcula e informa o valor do frete,
+a comissão do vendedor e o lucro obtido com a venda a partir
+das condições pré fornecidas. Destacando que acima de 1000 peças
+adquiridas há um desconto no frete proporcional à seu valor.'''
+
+#Entrada
+
+nome = input("Digite seu nome: ")
+local = float(input("Digite o código de sua região: "))
+num_pecas = float(input("Dígite o número de peças adquiridas: "))
+nome_vendedor = input("Digite o nome de seu vendedor: ") 
+
+
+
+#Processamento
+
+sul = 1
+norte = 2
+leste = 3
+oeste = 4
+
+
+if local == 1:
+    valor_frete = (num_pecas * 1.00)
+    if num_pecas > 1000:
+       valor_frete = (num_pecas * 0.9)
+
+if local == 2:
+    valor_frete = (num_pecas * 1.10)
+    if num_pecas > 1000:
+       valor_frete = (num_pecas * 1.0)
+
+if local == 3:
+    valor_frete = (num_pecas * 1.15)
+    if num_pecas > 1000:
+        valor_frete = (num_pecas * 1.10)
+
+
+if local == 4:
+    valor_frete = (num_pecas * 1.20)
+    if num_pecas > 1000:
+        valor_frete = (num_pecas * 1.15)
+        
+
+valor_pecas = (num_pecas * 7)
+
+valor_total = (valor_pecas + valor_pecas/2)
+
+comissao = (valor_total * 0.065)
+
+lucro = round(valor_total - valor_pecas - comissao,2)
+
+
+
+#Saída
+
+print("O valor do frete para sua região é de: ", valor_frete)
+print("O valor total de sua compra foi de: ", valor_total)
+print("Comissão do vendedor", nome_vendedor, "é de: ", comissao)
+print("Este é o lucro total: ", lucro)
